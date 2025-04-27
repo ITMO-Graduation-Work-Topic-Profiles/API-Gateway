16b0d984a15cd5db8aafbf4a72338c5e86fdd548
bezstrok <pocandre87@gmail.com>
2025-04-27 05:51:53 +0300

    Add config.py file to provide settings

---
+++ src/core/config.py
@@ -0,0 +1,41 @@
+import typing as tp
+
+from pydantic import AnyUrl, BaseModel, model_validator
+from pydantic_settings import BaseSettings
+
+__all__ = ["Settings"]
+
+class URLSchema(BaseModel):
+    scheme: str
+    host: str
+    port: int
+    username: str | None = None
+    password: str | None = None
+
+    _url: AnyUrl
+
+    @model_validator(mode="after")
+    def validate_url(self) -> tp.Self:
+        self._url = AnyUrl.build(
+            scheme=self.scheme,
+            host=self.host,
+            port=self.port,
+            username=self.username,
+            password=self.password,
+        )
+
+        return self
+
+    @property
+    def url(self) -> str:
+        return str(self._url)
+
+
+
+class MongoSchema(BaseModel):
+    connection: URLSchema
+    database: str
+
+class Settings(BaseSettings):
+    debug: bool
+    mongo: MongoSchema
