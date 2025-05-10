ebd3b7c8fc146a0aebc340d5b0beb519f197ca3c
bezstrok <pocandre87@gmail.com>
2025-05-11 01:30:59 +0300

    Refactor user transformation logic for topic profiles.

--- src/api/transformers/users.py
+++ src/api/transformers/users.py
@@ -11,7 +11,12 @@ __all__ = [
 def get_user_with_topic_profile_repository_to_user_get_dto_transformer(
     item: tp.Any,
 ) -> UserGetDTO:
-    return UserGetDTO.model_validate(item)
+    return UserGetDTO(
+        user_id=item["user_id"],
+        username=item["username"],
+        # Nested structure because of unwind on the pipeline level
+        topic_profile=item["topic_profile"]["topic_profile"],
+    )
 
 
 def get_users_with_topic_profiles_paginated_repository_to_user_get_dto_transformer(
